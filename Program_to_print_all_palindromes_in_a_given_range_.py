a93c700f331a7c845cda884dfdac9f21de290610
Teja8899 <99643609+Teja8899@users.noreply.github.com>
2022-05-16 20:40:30 +0530

    n=int(input()) o=int(input()) for i in range(n,o+1,1):     t=i     s=0     while(i>0):         r=i%10         s=s*10+r         i=i//10     if(t==s):         print(t,end=' ')

---
+++ Program_to_print_all_palindromes_in_a_given_range_.py
@@ -0,0 +1,12 @@
+n=int(input())
+o=int(input())
+for i in range(n,o+1,1):
+    t=i
+    s=0
+    while(i>0):
+        r=i%10
+        s=s*10+r
+        i=i//10
+    if(t==s):
+        print(t,end=' ')
+    
