7da09fb9c198ad7a242e515aa9a442955f272921
ROTTE BALA SURYA VENKATA TEJA <99643609+Teja8899@users.noreply.github.com>
2022-07-18 10:31:07 +0530

    def prime(a):     c=0     for i in range(1,a+1):         if a%i==0:             c+=1     if c==2:         return 1     else:         return 0 a=int(input()) b=int(input()) for i in range(a,b+1):     if prime(i)==1:         print(i)

--- Generate_Prime_Numbers_In_A_Interval_.py
+++ Generate_Prime_Numbers_In_A_Interval_.py
@@ -1,9 +1,14 @@
+def prime(a):
+    c=0
+    for i in range(1,a+1):
+        if a%i==0:
+            c+=1
+    if c==2:
+        return 1
+    else:
+        return 0
 a=int(input())
 b=int(input())
-for i in range(a,b+1,1):
-    m=0
-    for j in range(1,i+1,1):
-        if i%j==0:
-            m+=1
-    if m==2:
+for i in range(a,b+1):
+    if prime(i)==1:
         print(i)
